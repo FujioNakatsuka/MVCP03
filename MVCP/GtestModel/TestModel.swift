b6f387d43d15b003d46e21f45e6e2d0e4a8188d1
Fujio Nakatsuka <com.nifty.cbm01799>
2020-07-25 00:31:46 +0900

    name/ageのボタンを押すとname1/age1がラベルに表示されるようにしたいが、person1.name/person1.ageではダメ！さて。。。

---
+++ MVCP/GtestModel/TestModel.swift
@@ -0,0 +1,43 @@
+//
+//  TestModel.swift
+//  MVCP
+//
+//  Created by 中塚富士雄 on 2020/07/24.
+//  Copyright © 2020 Yuta Fujii. All rights reserved.
+//
+
+import Foundation
+
+//struct TestModel {
+//    var word1 = String()
+//    var word2 = String()
+//
+//}
+
+
+struct Person {
+    
+    var name = String()
+    var age = String()
+    
+}
+
+
+
+
+
+
+//class TestModel{
+//
+//    var test1 = String()
+//    var test2 = String()
+//
+//    init(word1:String,word2:String) {
+//        test1 = word1
+//        test2 = word2
+//    }
+//    func stringwa() -> String {
+//        return test1 + test2
+//    }
+//
+//}
--- MVCP/ViewController.swift
+++ MVCP/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    @IBOutlet weak var Data: UILabel!
+    
+     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+         /*
+        Personクラスを作成し、構造体でname、ageプロパティーを作成した後、
+        ViewController1.swiftでpersonというインスタンスをそれぞれ異なる名前、年齢を用いて、
+        ラベルへ表示させてください。
+        */
+        
+        var person1 = Person(name:"name1", age:"age1" )
+        
+        print(person1.age)
+        print(person1.name)
+        
+        
+        
+        
+//        var testModel = TestModel(word1: "abcd", word2: "efgh")
+//        print(testModel.word1 + testModel.word2)
+        
+        
+        
+//        var testModel = TestModel(word1: "abcd", word2: "efgh")
+//        var wa = testModel.stringwa()
+//        print(wa)
     }
 
+    @IBAction func BtnName(_ sender: Any) {
+        
+        Data.text = person1.name
+    }
+    @IBAction func BtnAge(_ sender: Any) {
+        Data.text = person1.age
 
+    }
+    
 }
 
